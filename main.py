c15bce6e43299332e276015ee62979f49247dd20
Anish <nkanish2002@gmail.com>
2018-02-05 15:46:56 -0500

    Adding google vision api

--- main.py
+++ main.py
@@ -8,16 +8,20 @@ import subprocess
 from time import time
 from PIL import Image
 
+
 def conf_exists():
     return os.path.isfile("conf.js")
+
 
 def update_conf(conf):
     f = open("conf.js", 'w')
     f.write(json.dumps(conf, indent=4))
     f.close()
 
+
 def get_conf():
     return json.loads(open("conf.js", 'r').read())
+
 
 def get_api():
     if not conf_exists():
@@ -25,7 +29,7 @@ def get_api():
         exit(1)
     conf = get_conf()
     auth = tweepy.OAuthHandler(conf["twitter"]["consumer_key"],
-                                conf["twitter"]["consumer_secret"])
+                               conf["twitter"]["consumer_secret"])
     if "access_token" in conf and "access_token_secret" in conf:
         print("Using cached user info")
         auth.set_access_token(conf["access_token"], conf["access_token_secret"])
@@ -43,39 +47,42 @@ def get_api():
     api = tweepy.API(auth)
     return api
 
+
 def get_media_files(api, screen_name):
     # screen_name = input("Enter the User ID: ")
     tweets = api.user_timeline(screen_name=screen_name,
-                           count=20, include_rts=False,
-                           exclude_replies=True)
+                               count=20, include_rts=False,
+                               exclude_replies=True)
     last_id = tweets[-1].id
     count = 0
     while (count < 3000):
         more_tweets = api.user_timeline(screen_name=screen_name,
-                                    count=20,
-                                    include_rts=False,
-                                    exclude_replies=True,
-                                    max_id=last_id-1)
+                                        count=20,
+                                        include_rts=False,
+                                        exclude_replies=True,
+                                        max_id=last_id - 1)
         count += len(more_tweets)
         print("Read through %d tweets" % count)
         # There are no more tweets
         if (len(more_tweets) == 0):
             break
         else:
-            last_id = more_tweets[-1].id-1
+            last_id = more_tweets[-1].id - 1
             tweets = tweets + more_tweets
 
     media_files = set()
     for status in tweets:
         media = status.entities.get('media', [])
-        if(len(media) > 0):
+        if (len(media) > 0):
             media_files.add(media[0]['media_url'])
     return media_files
+
 
 def resize_image(image_name, size):
     im = Image.open(image_name)
     im.thumbnail(size, Image.ANTIALIAS)
     im.save(image_name)
+
 
 def download_files(media_files, folder_name, limit=1000):
     if media_files:
@@ -83,15 +90,18 @@ def download_files(media_files, folder_name, limit=1000):
             os.mkdir(folder_name)
         for i, media_file in enumerate(media_files):
             if i < limit:
-                path = "%s/pic%04d.jpg" % (folder_name, i+1)
+                path = "%s/pic%04d.jpg" % (folder_name, i + 1)
                 wget.download(media_file, out=path)
-                resize_image(path, (600,600))
+                resize_image(path, (600, 600))
             else:
                 break
 
+
 def create_movie(name, folder):
-    cmd = ["ffmpeg", "-framerate", "1", "-i", folder +"/pic%04d.jpg", "-c:v", "libx264", "-r", "30", "-pix_fmt", "yuv420p", name]
+    cmd = ["ffmpeg", "-framerate", "1", "-i", folder + "/pic%04d.jpg", "-c:v",
+           "libx264", "-r", "30", "-pix_fmt", "yuv420p", name]
     return subprocess.call(cmd)
+
 
 def main():
     print("Starting process")
@@ -107,8 +117,9 @@ def main():
     if media_files:
         tmp_dir = "tmp_%d" % (int(time()))
         download_files(media_files, tmp_dir)
-        create_movie(tmp_dir+".mp4", tmp_dir)
+        create_movie(tmp_dir + ".mp4", tmp_dir)
         os.rmdir(tmp_dir)
+
 
 if __name__ == "__main__":
     main()
